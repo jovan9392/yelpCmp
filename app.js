31320d9e9af39dd88ee43b901fe39e26737d47b1
jovan9392 <121186826+jovan9392@users.noreply.github.com>
2023-01-02 20:44:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/reviews');
 const User = require('./models/user');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl)
     .then(() => {
